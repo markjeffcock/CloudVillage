406fa5b58c2715a70cca064fc191811b03aa947c
markjeffcock <mark.jeffcock@gmail.com>
2020-11-05 13:08:05 +0100

    Starter WinePour script

---
+++ Cloud Village/Assets/Scripts/PourWine.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PourWine : MonoBehaviour
+{
+    public int poutThreshold = 90;
+    public Transform origin = null;
+    public GameObject wineStream = null;
+
+    private bool isPouring = false;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void StartPour()
+    {
+
+    }
+    // Update is called once per frame
+    private void EndPour()
+    {
+
+    }
+}

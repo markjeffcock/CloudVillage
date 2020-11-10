420fb2dbda90a2c1079c8869e22c2a090d43c72c
markjeffcock <mark.jeffcock@gmail.com>
2020-11-10 13:01:34 +0100

    TeleportationAreaGlobal.cs

---
+++ Cloud Village/Assets/Scripts/TeleportationAreaGlobal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+namespace UnityEngine.XR.Interaction.Toolkit
+{
+    public class TeleportationAreaGlobal : BaseTeleportationInteractable
+    {
+
+        protected override bool GenerateTeleportRequest(XRBaseInteractor interactor, RaycastHit raycastHit, ref TeleportRequest teleportRequest)
+        {
+            teleportRequest.destinationPosition = raycastHit.point;
+            teleportRequest.destinationUpVector = Vector3.up; // use the area transform for data.
+            teleportRequest.destinationForwardVector = Vector3.forward;
+            teleportRequest.destinationRotation = Quaternion.identity;
+            return true;
+        }
+    }
+}

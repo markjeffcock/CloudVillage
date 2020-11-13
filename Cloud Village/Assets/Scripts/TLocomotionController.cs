e261b18db66a2cff3e4d6764f0cd284ff1ffd0c8
markjeffcock <mark.jeffcock@gmail.com>
2020-11-13 11:14:51 +0100

    Attempt fix on TLocomotionController

--- Cloud Village/Assets/Scripts/TLocomotionController.cs
+++ Cloud Village/Assets/Scripts/TLocomotionController.cs
@@ -9,19 +9,29 @@ public class TLocomotionController : MonoBehaviour
     public XRController rightTeleportRay;
     public InputHelpers.Button teleportActivationButton;
     public float activationThreshold = 0.1f;
+    private XRRayInteractor rightRayInteractor; 
+    private XRRayInteractor leftRayInteractor;
 
-
+    private void Start()
+    {
+        if (rightTeleportRay) 
+             rightRayInteractor = rightTeleportRay.gameObject.GetComponent<XRRayInteractor>(); 
+        if (leftTeleportRay) 
+             leftRayInteractor = leftTeleportRay.gameObject.GetComponent<XRRayInteractor>(); 
+    }
 
     // Update is called once per frame
     void Update()
     {
        if(leftTeleportRay)
         {
+            leftRayInteractor.allowSelect = CheckIfActivated(leftTeleportRay); 
             leftTeleportRay.gameObject.SetActive(CheckIfActivated(leftTeleportRay));
         }
 
         if (rightTeleportRay)
         {
+            rightRayInteractor.allowSelect = CheckIfActivated(rightTeleportRay); 
             rightTeleportRay.gameObject.SetActive(CheckIfActivated(rightTeleportRay));
         }
     }

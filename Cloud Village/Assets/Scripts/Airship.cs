5ee87e7cbb8372e7e19a52eb6da15e84ab5f6427
markjeffcock <mark.jeffcock@gmail.com>
2020-12-05 18:38:50 +0100

    Basic airship script

---
+++ Cloud Village/Assets/Scripts/Airship.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Airship : MonoBehaviour
+{
+    private float baseSpeed = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // We'll move the airship forward at a basic speed
+        transform.Translate(Vector3.forward * Time.deltaTime * baseSpeed);
+
+    }
+}

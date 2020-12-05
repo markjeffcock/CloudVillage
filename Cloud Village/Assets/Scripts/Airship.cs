8edb7ad3248cb1e66bf06a9c51bcbd14d3a18f71
markjeffcock <mark.jeffcock@gmail.com>
2020-12-05 22:36:02 +0100

    Hinge Stand to Boat
    Minor fies to lampposts; steps; stairs

--- Cloud Village/Assets/Scripts/Airship.cs
+++ Cloud Village/Assets/Scripts/Airship.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Airship : MonoBehaviour
 {
     private float baseSpeed = 1.0f;
+    private float baseElevate = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,5 +18,7 @@ public class Airship : MonoBehaviour
         // We'll move the airship forward at a basic speed
         transform.Translate(Vector3.left * Time.deltaTime * baseSpeed);
 
+        // We'll move the airship up at a basic speed
+        transform.Translate(Vector3.up * Time.deltaTime * baseElevate);
     }
 }

3cfafe7a39385d224395fb36afba594a8311f26a
markjeffcock <mark.jeffcock@gmail.com>
2020-12-08 20:50:13 +0100

    Movement only when on board!

--- Cloud Village/Assets/Scripts/Airship.cs
+++ Cloud Village/Assets/Scripts/Airship.cs
@@ -6,19 +6,22 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class Airship : MonoBehaviour
 {
-    private float baseSpeed = 0.1f;
-    private float baseElevate = 0.0f;
+    private float baseSpeed = 0.2f;
+    private float baseElevate = 0.2f;
 
     public Transform upDown;
     public Transform leftRight;
     public Transform forwardBack;
 
     public CharacterController character;
+    public Collider onBoard;
 
     private Vector3 lastFramePosition;
     private Quaternion lastFrameRotation;
     private Vector3 vehicleMovement;
     private Quaternion vehicleRotation;
+
+    private Vector3 currentPlayerPosition;
 
     // Start is called before the first frame update
     // Initialise values to avoid nulls
@@ -32,32 +35,37 @@ public class Airship : MonoBehaviour
     void FixedUpdate()
 
     {
-        Vector3 direction = new Vector3(baseSpeed, baseElevate, 0);
-        // Vehicle movement since last frame
-        vehicleMovement = transform.position - lastFramePosition;
-        vehicleRotation = transform.rotation * Quaternion.Inverse(lastFrameRotation);
+        currentPlayerPosition = character.gameObject.transform.position;
 
-        // Capture current position
-        lastFramePosition = transform.position;
-        lastFrameRotation = transform.rotation;
+        // Only Move if someone on Board
 
-        // We'll move the airship forward at a basic speed
-        transform.Translate(Vector3.left * Time.fixedDeltaTime * baseSpeed);
+        if (onBoard.bounds.Contains(currentPlayerPosition))
+        {
+            // Vehicle movement since last frame
+            vehicleMovement = transform.position - lastFramePosition;
+            vehicleRotation = transform.rotation * Quaternion.Inverse(lastFrameRotation);
 
-        // We'll move the airship up at a basic speed
-        transform.Translate(Vector3.up * Time.fixedDeltaTime * baseElevate);
+            // Capture current position
+            lastFramePosition = transform.position;
+            lastFrameRotation = transform.rotation;
 
-        // Move character (better if we only WHEN ON Airship)
-        // Character MOVe not working when we capture small current positions (as above) 
-        // Idea A (not with deltaTime)
-        // Idea: change to Move to Wheel Position (after making this work ONLY when on Airship)
-        // Idea 2: try character.SImpleMove
-        // Idea 3: try transform of character to WheelPosition
-        // TempIdea - large box collider behind character.
+            // We'll move the airship forward at a basic speed
+            transform.Translate(Vector3.left * Time.fixedDeltaTime * baseSpeed);
 
-        // Initial version character.Move(vehicleMovement * Time.fixedDeltaTime); 
-        character.Move(vehicleMovement);
+            // We'll move the airship up at a basic speed
+            transform.Translate(Vector3.up * Time.fixedDeltaTime * baseElevate);
 
+            // Move character (better if we only WHEN ON Airship)
+            // Character MOVe not working when we capture small current positions (as above) 
+            // Idea A (not with deltaTime)
+            // Idea: change to Move to Wheel Position (after making this work ONLY when on Airship)
+            // Idea 2: try character.SImpleMove
+            // Idea 3: try transform of character to WheelPosition
+            // TempIdea - large box collider behind character.
+
+            // Initial version character.Move(vehicleMovement * Time.fixedDeltaTime); 
+            character.Move(vehicleMovement);
+        }
 
     }
 }

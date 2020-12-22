b83d952299ad2be7c456168ab0314709b8af4054
markjeffcock <mark.jeffcock@gmail.com>
2020-12-22 23:12:55 +0100

    Collected ViewId of NetworkPlyer(Clone)

--- Cloud Village/Assets/Scripts/NetworkPlayerSpawner.cs
+++ Cloud Village/Assets/Scripts/NetworkPlayerSpawner.cs
@@ -6,11 +6,18 @@ using Photon.Pun;
 public class NetworkPlayerSpawner : MonoBehaviourPunCallbacks
 {
     private GameObject spawnedPlayerPrefab;
+    private PhotonView pView;
+    public int numberInWorld = 0;
     // 
     public override void OnJoinedRoom()
     {
         base.OnJoinedRoom();
         spawnedPlayerPrefab = PhotonNetwork.Instantiate("Network Player", transform.position, transform.rotation);
+        pView = spawnedPlayerPrefab.GetComponent<PhotonView>();
+
+        numberInWorld = pView.ViewID;
+
+        Debug.Log(numberInWorld);
 
         //this would be where to change colour or add number to spawnedPlayerPrefab
     }

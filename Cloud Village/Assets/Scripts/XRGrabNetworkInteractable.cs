d189335a3c017eef53fb7d6a512d1d163dafc84c
markjeffcock <mark.jeffcock@gmail.com>
2021-01-07 15:59:18 +0100

    Update XRGrabNetworkInteractable.cs

--- Cloud Village/Assets/Scripts/XRGrabNetworkInteractable.cs
+++ Cloud Village/Assets/Scripts/XRGrabNetworkInteractable.cs
@@ -7,12 +7,25 @@ using Photon.Pun;
 
 public class XRGrabNetworkInteractable : XRGrabInteractable
 {
-    private PhotonView photonView;
+    private PhotonView photonView; 
+    private Vector3 initialAttachLocalPos;
+    private Quaternion initialAttachLocalRot;
 
     // Start is called before the first frame update
     void Start()
     {
-        photonView = GetComponent<PhotonView>();
+        photonView = GetComponent<PhotonView>(); 
+        
+        // Create attache Transform (if not existing
+        if (!attachTransform)
+        {
+            GameObject grab = new GameObject("Grab Pivot");
+            grab.transform.SetParent(transform, false);
+            attachTransform = grab.transform;
+        }
+
+        initialAttachLocalPos = attachTransform.localPosition;
+        initialAttachLocalRot = attachTransform.localRotation;
     }
 
     // Update is called once per frame
@@ -23,7 +36,19 @@ public class XRGrabNetworkInteractable : XRGrabInteractable
 
     protected override void OnSelectEnter(XRBaseInteractor interactor)
     {
-        photonView.RequestOwnership();
+
+        if (interactor is XRDirectInteractor)
+        {
+            attachTransform.position = interactor.transform.position;
+            attachTransform.rotation = interactor.transform.rotation;
+        }
+        else
+        {
+            attachTransform.localPosition = initialAttachLocalPos;
+            attachTransform.localRotation = initialAttachLocalRot;
+        }
+
+        photonView.RequestOwnership(); 
         base.OnSelectEnter(interactor);
     }
 }

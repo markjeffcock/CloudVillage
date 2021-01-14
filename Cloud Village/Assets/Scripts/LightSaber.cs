6741743e46c0b4027ffe197d13bce8f0f77bf4e1
markjeffcock <mark.jeffcock@gmail.com>
2021-01-14 17:08:40 +0100

    v116. Some Balloon movement
    LightSaber on needs adjustment.
    Balloon only goes forward.
    Airship Handle still not working.
    Gondola apparently good with Photon.

--- Cloud Village/Assets/Scripts/LightSaber.cs
+++ Cloud Village/Assets/Scripts/LightSaber.cs
@@ -8,6 +8,14 @@ public class LightSaber : MonoBehaviour
     public void TurnOnLightSaber()
     {
         LightSaberAnimator.SetTrigger("Turn Light Saber On");
+        LightSaberAnimator.ResetTrigger("Turn Light Saber Off");
+
+    }
+
+    public void TurnOffLightSaber()
+    {
+        LightSaberAnimator.SetTrigger("Turn Light Saber Off");
+        LightSaberAnimator.ResetTrigger("Turn Light Saber On");
 
     }
 }

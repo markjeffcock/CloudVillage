9aced6e54a9ad2228a5f5fb9c53892f5d2976d81
markjeffcock <mark.jeffcock@gmail.com>
2021-01-14 10:39:48 +0100

    Added initial LightSaber animation

---
+++ Cloud Village/Assets/Scripts/LightSaber.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSaber : MonoBehaviour
+{
+    public Animator LightSaberAnimator;
+    public void TurnOnLightSaber()
+    {
+        LightSaberAnimator.SetTrigger("Turn Light Saber On");
+
+    }
+}
